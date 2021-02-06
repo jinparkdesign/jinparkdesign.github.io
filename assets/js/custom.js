994e9c9e8190ef81b420e2fea083ea4522741bb6
Jin Park <jinizzuru@gmail.com>
2021-02-06 00:13:17 -0500

    Suggest next project, resume download

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,41 +7,139 @@ var win = $(window);
 var ww = win.width();
 var wh = win.height();
 
-$(document).ready(function() {
+let suggestedProjects = [{
+        id: 0,
+        link: "Android-3d",
+        img: "3d.gif",
+        title: "\"Android 3D Modeling\"",
+        subtitle: "3D & Animation | 2020"
+    },
+    {
+        id: 1,
+        link: "conneqtion",
+        img: "UX3.png",
+        title: "\"ConneQtion Mobile App\"",
+        subtitle: "UX/UI Design | 2020"
+    },
+    {
+        id: 2,
+        link: "hug-mug",
+        img: "brandbook.png",
+        title: "\"HUG MUG Brand Book Design\"",
+        subtitle: "Branding & Layout | 2020"
+    },
+    {
+        id: 3,
+        link: "project",
+        img: "portfolio-1.png",
+        title: "eCycle App Design",
+        subtitle: "UX/UI Design | 2020"
+    },
+    {
+        id: 4,
+        link: "signage",
+        img: "sign.jpg",
+        title: "\"Signage Design @ Fastsigns\"",
+        subtitle: "Graphic & Print Design |2016-2018"
+    },
+    {
+        id: 5,
+        link: "tiktok-logo-reveal",
+        img: "tiktok.gif",
+        title: "\"Tiktok Logo Reveal\"",
+        subtitle: "Video Editing & Motion Design | 2020"
+    },
+    {
+        id: 6,
+        link: "when-you-drive",
+        img: "drive1.gif",
+        title: "\"Park Your Phone\"",
+        subtitle: "Graphic & Motion Design | 2020"
+    }
+];
+
+let numProjectsToShow = 2;
+
+const Item = ({
+    link,
+    img,
+    title,
+    subtitle
+}) => `
+<div class="col-md-6 portfolio-item filter-3d">
+<div class="portfolio-wrap" onclick="openProject('${link}')" style="height: 357px; overflow: hidden;">
+  <img src="../assets/img/portfolio/${img}" class="img-fluid" alt="">
+  <div class="portfolio-info">
+    <h4>${title}</h4>
+    <p>${subtitle}</p>
+
+  </div>
+</div>
+</div>
+`;
+
+
+$(document).ready(function () {
 
     // load functions
     imageBG();
     grid();
+    let projects = [];
+    // get current project by url
+    let url = window.location.href;
+    let urlArray = url.split('/');
+    let fileUrl = urlArray[urlArray.length - 1];
 
+    // search in suggestedProjects for current page
+    suggestedProjects.find((o, i) => {
+        let currentLink = o.link + '.html';
+        if (currentLink === fileUrl) {
+            // remove
+            suggestedProjects.splice(i, 1);
+            return true; // stop searching
+        }
+    });
+
+
+    for (let i = 0; i < numProjectsToShow; i++) {
+        let random = generateRandomNumber();
+        projects.push(suggestedProjects[random]);
+        // remove from suggestedProject
+        suggestedProjects.splice(random, 1);
+    }
+
+    for (let i = 0; i < projects.length; i++) {
+        $('.suggested-project').html(projects.map(Item).join(''));
+    }
 });
 
-win.on('load', function() {
+win.on('load', function () {
 
-    setTimeout(function() {
+    setTimeout(function () {
         $('#preloader').addClass('hide');
     }, 1000);
 
     // load functions
     grid();
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $(document).on('click', '.mobile-nav-toggle', function (e) {
         $('body').toggleClass('mobile-nav-active');
         $('#header').toggleClass('header-animation');
         $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-      });
-    
-      $(document).click(function(e) {
+    });
+
+    $(document).click(function (e) {
         var container = $(".mobile-nav-toggle");
         if (!container.is(e.target) && container.has(e.target).length === 0) {
-          if ($('body').hasClass('mobile-nav-active')) {
-            $('body').removeClass('mobile-nav-active');
-            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-          }
+            if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+            }
         }
-      });
+    });
 });
 
-win.on('resize', function() {
+win.on('resize', function () {
 
     // viewport dimensions
     ww = win.width();
@@ -49,7 +147,7 @@ win.on('resize', function() {
 
     // load functions
     grid();
-    
+
 
 });
 
@@ -62,7 +160,7 @@ function show_hide_header() {
 
     var last_scroll = 0;
 
-    win.on('scroll', function() {
+    win.on('scroll', function () {
         if (!$('#about').hasClass('visible')) {
             var scroll = $(this).scrollTop();
 
@@ -85,7 +183,7 @@ function show_hide_header() {
 
 function imageBG() {
 
-    $('.imageBG').each(function() {
+    $('.imageBG').each(function () {
         var image = $(this).data('img');
 
         $(this).css({
@@ -135,13 +233,13 @@ function grid() {
         var items_double_height = items_height * double_height;
         var items_margin = Math.floor(margin / 2);
 
-        items.each(function() {
+        items.each(function () {
             $(this).css('width', items_width + 'px');
             $(this).css('height', items_height + 'px');
             $(this).css('margin', items_margin + 'px');
 
             if (!height) $(this).css('height', 'auto');
-            if ($(this).hasClass('w2') && ww >= 500) $(this).css('width', (items_width * 2) + (items_margin * 2) + 'px');  /* Add w2 or h2 to the portfolio item for varoius layout sizes */
+            if ($(this).hasClass('w2') && ww >= 500) $(this).css('width', (items_width * 2) + (items_margin * 2) + 'px'); /* Add w2 or h2 to the portfolio item for varoius layout sizes */
             if ($(this).hasClass('h2') && ww >= 500) $(this).css('height', items_double_height + (items_margin * 2) + 'px');
         });
 
@@ -157,11 +255,11 @@ function grid() {
             },
             masonry: {
                 columnWidth: items_width + margin
-                
+
             }
         });
 
-        $('#filters li a').on('click', function(e) {
+        $('#filters li a').on('click', function (e) {
             e.preventDefault();
 
             var filter = $(this).attr('href');
@@ -177,6 +275,12 @@ function grid() {
 
 }
 
+// Generate Random
+function generateRandomNumber() {
+    let max = suggestedProjects.length;
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-
-
+let openProject = function (projectName) {
+    location.href = `./${projectName}.html`;
+}
